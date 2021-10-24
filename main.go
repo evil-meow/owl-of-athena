a88a06c35d98dba91fa5048ee0e7a1a8bd1435b5
Rafa de Castro <rafael@micubiculo.com>
2021-10-24 12:27:20 +0200

    Starting to break in packages

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
+	"evil-meow/owl-of-athena/handlers"
 	"log"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
@@ -72,49 +70,6 @@ func main() {
 	socketClient.Run()
 }
 
-// handleAppMentionEvent is used to take care of the AppMentionEvent when the bot is mentioned
-func handleAppMentionEvent(event *slackevents.AppMentionEvent, client *slack.Client) error {
-
-	// Grab the user name based on the ID of the one who mentioned the bot
-	user, err := client.GetUserInfo(event.User)
-	if err != nil {
-		return err
-	}
-	// Check if the user said Hello to the bot
-	text := strings.ToLower(event.Text)
-
-	// Create the attachment and assigned based on the message
-	attachment := slack.Attachment{}
-	// Add Some default context like user who mentioned the bot
-	attachment.Fields = []slack.AttachmentField{
-		{
-			Title: "Date",
-			Value: time.Now().String(),
-		}, {
-			Title: "Initializer",
-			Value: user.Name,
-		},
-	}
-	if strings.Contains(text, "hello") {
-		// Greet the user
-		attachment.Text = fmt.Sprintf("Hello %s", user.Name)
-		attachment.Pretext = "Greetings"
-		attachment.Color = "#4af030"
-	} else {
-		// Send a message to the user
-		attachment.Text = fmt.Sprintf("How can I help you %s?", user.Name)
-		attachment.Pretext = "How can I be of service"
-		attachment.Color = "#3d3d3d"
-	}
-	// Send the message to the channel
-	// The Channel is available in the event message
-	_, _, err = client.PostMessage(event.Channel, slack.MsgOptionAttachments(attachment))
-	if err != nil {
-		return fmt.Errorf("failed to post message: %w", err)
-	}
-	return nil
-}
-
 // handleEventMessage will take an event and handle it properly based on the type of event
 func handleEventMessage(event slackevents.EventsAPIEvent, client *slack.Client) error {
 	switch event.Type {
@@ -126,7 +81,7 @@ func handleEventMessage(event slackevents.EventsAPIEvent, client *slack.Client) 
 		switch ev := innerEvent.Data.(type) {
 		case *slackevents.AppMentionEvent:
 			// The application has been mentioned since this Event is a Mention event
-			err := handleAppMentionEvent(ev, client)
+			err := handlers.HandleAppMentionEvent(ev, client)
 			if err != nil {
 				return err
 			}
