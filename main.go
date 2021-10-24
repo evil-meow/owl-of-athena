96e1ff1a5baf9e065a7b46d88fddc37e9334cbc9
Rafa de Castro <rafael@micubiculo.com>
2021-10-24 13:02:37 +0200

    Some unnecessary comments

--- main.go
+++ main.go
@@ -44,33 +44,27 @@ func main() {
 				// We have a new Events, let's type switch the event
 				// Add more use cases here if you want to listen to other events.
 				switch event.Type {
-				// handle EventAPI events
 				case socketmode.EventTypeEventsAPI:
-					// The Event sent on the channel is not the same as the EventAPI events so we need to type cast it
 					eventsAPIEvent, ok := event.Data.(slackevents.EventsAPIEvent)
 					if !ok {
 						log.Printf("Could not type cast the event to the EventsAPIEvent: %v\n", event)
 						continue
 					}
-					// We need to send an Acknowledge to the slack server
+
 					socketClient.Ack(*event.Request)
-					// Now we have an Events API event, but this event type can in turn be many types, so we actually need another type switch
 					err := handlers.HandleEventMessage(eventsAPIEvent, client)
 					if err != nil {
-						// Replace with actual err handeling
 						log.Fatal(err)
 					}
-					// Handle Slash Commands
+
 				case socketmode.EventTypeSlashCommand:
-					// Just like before, type cast to the correct event type, this time a SlashEvent
 					command, ok := event.Data.(slack.SlashCommand)
 					if !ok {
 						log.Printf("Could not type cast the message to a SlashCommand: %v\n", command)
 						continue
 					}
-					// Dont forget to acknowledge the request
+
 					socketClient.Ack(*event.Request)
-					// handleSlashCommand will take care of the command
 					err := handlers.HandleSlashCommand(command, client)
 					if err != nil {
 						log.Fatal(err)
