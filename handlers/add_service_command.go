52a65731db1453b40ce2531bf2e6b4182d3bfc5c
Rafa de Castro <rafael@micubiculo.com>
2021-10-26 11:39:06 +0200

    Creating repo if needed

--- handlers/add_service_command.go
+++ handlers/add_service_command.go
@@ -10,36 +10,45 @@ import (
 
 // handleHelloCommand will take care of /add_service submissions
 func HandleAddServiceCommand(command slack.SlashCommand, client *slack.Client) error {
-	// The Input is found in the text field so
-	// Create the attachment and assigned based on the message
+
+	serviceName := &command.Text
+	username := &command.UserName
+	channelID := &command.ChannelID
+
+	sendMessage(client, username, channelID, serviceName, fmt.Sprintf("Adding the service at %s", serviceName))
+
+	if github_api.IsGithubRepoCreated(*serviceName) {
+		sendMessage(client, username, channelID, serviceName, fmt.Sprintf("Repo %s exists. I won't do anything.", serviceName))
+		return nil
+	} else {
+		sendMessage(client, username, channelID, serviceName, fmt.Sprintf("Repo %s does not exist. Creating.", serviceName))
+		github_api.CreateGitubRepo(serviceName)
+	}
+
+	return nil
+}
+
+func sendMessage(client *slack.Client, username *string, channelID *string, serviceName *string, text string) {
 	attachment := slack.Attachment{}
-	// Add Some default context like user who mentioned the bot
+
 	attachment.Fields = []slack.AttachmentField{
 		{
 			Title: "Date",
 			Value: time.Now().String(),
 		}, {
 			Title: "Initializer",
-			Value: command.UserName,
+			Value: *username,
+		}, {
+			Title: "Service",
+			Value: *serviceName,
 		},
 	}
 
-	serviceName := command.Text
-
-	// Acknowledge that the request was received
-	attachment.Text = fmt.Sprintf("Adding the service at %s", serviceName)
+	attachment.Text = text
 	attachment.Color = "#4af030"
 
-	_, _, err := client.PostMessage(command.ChannelID, slack.MsgOptionAttachments(attachment))
+	_, _, err := client.PostMessage(*channelID, slack.MsgOptionAttachments(attachment))
 	if err != nil {
-		return fmt.Errorf("failed to post message: %w", err)
+		fmt.Errorf("failed to post message: %w", err)
 	}
-
-	if github_api.IsGithubRepoCreated(serviceName) {
-		fmt.Sprintf("Repo %s exists", serviceName)
-	} else {
-		fmt.Sprintf("Repo %s does not exist", serviceName)
-	}
-
-	return nil
 }
